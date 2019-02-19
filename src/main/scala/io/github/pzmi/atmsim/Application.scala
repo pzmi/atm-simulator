0ce7b30367c8858ac8da2a6e0716f3854a90782c
Patryk Zmigrodzki <patryk.zmigrodzki@allegro.pl>
2019-02-20 00:31:59 +0100

    Add webapp

--- src/main/scala/io/github/pzmi/atmsim/Application.scala
+++ src/main/scala/io/github/pzmi/atmsim/Application.scala
@@ -7,8 +7,8 @@ import com.typesafe.scalalogging.StrictLogging
 object Application extends App with ActorModule with ServerModule with StrictLogging {
   Simulation.start(1L, 1000, 1000000)
 
-  val appRoute = pathPrefix("app")(getFromResource("index.html"))
-  val staticRoute = pathPrefix("app/static")(getFromResourceDirectory("static"))
+  val appRoute = getFromResource("index.html")
+  val staticRoute = pathPrefix("static")(getFromResourceDirectory("static"))
 
   val bindingFuture = Http().bindAndHandle(appRoute ~ staticRoute, "localhost", 8080)
 
