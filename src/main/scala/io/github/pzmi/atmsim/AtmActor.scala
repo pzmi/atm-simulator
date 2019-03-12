ce9f49f93be5f56512956a21ff471c089afa777f
pzmi <p.zmigrodzki@gmail.com>
2019-03-12 23:49:02 +0100

    Rework atm actor

--- src/main/scala/io/github/pzmi/atmsim/AtmActor.scala
+++ src/main/scala/io/github/pzmi/atmsim/AtmActor.scala
@@ -18,69 +18,62 @@ object AtmActor {
 class AtmActor(output: ActorRef) extends Actor with ActorLogging {
   private val startingBalance: Int = 10000
 
-  override def receive: Receive = functional(startingBalance)
+  override def receive: Receive = operational(startingBalance)
 
-  def outOfMoney: Receive = {
-    case e: Event => sendToOutputAndAck(0, OutOfMoney(e.time))
+  private def outOfMoney: Receive = {
+    case w: Withdrawal => sendToOutputAndAck(0, w)
+      sendToOutputAndAck(0, OutOfMoney(w.time))
   }
 
-  private def functional(currentBalance: Int): Receive = {
+  private def operational(currentBalance: Int): Receive = {
     case w: Withdrawal =>
-      val newBalance = currentBalance - w.amount
-
-      if (newBalance < 0) {
-        val notEnoughMoney = NotEnoughMoney(w.time, balance = currentBalance)
-        sendToOutputAndAck(currentBalance, notEnoughMoney)
-      } else {
-        sendToOutputAndAck(newBalance, w)
-        if (newBalance == 0) {
-          sendToOutputAndAck(newBalance, OutOfMoney(w.time))
-          context.become(outOfMoney)
-        } else {
-          context.become(functional(newBalance))
-        }
-      }
-
-    case e: Event =>
-      sendToOutputAndAck(e)
+      val newBalance: Int = calculateBalance(currentBalance, w)
+      sendToOutputAndAck(newBalance, w)
   }
 
-  private def sendToOutputAndAck(e: Event): Unit = {
-    implicit val timeout: Timeout = Timeout(30 seconds)
-    Await.result(output ? e, 30 seconds)
-    sender() ! Done
-  }
+  private def calculateBalance(currentBalance: Int, w: Withdrawal): Int =
+    currentBalance - w.amount match {
+      case newBalance if newBalance < 0 => sendToOutputAndAck(currentBalance, NotEnoughMoney(w.time))
+        currentBalance
+      case newBalance if newBalance == 0 => sendToOutputAndAck(newBalance, OutOfMoney(w.time))
+        context.become(outOfMoney)
+        newBalance
+      case newBalance if newBalance > 0 => newBalance
+    }
 
   private def sendToOutputAndAck(newBalance: Int, event: Event): Unit = {
     sendToOutputAndAck(updateBalanceAndAtm(newBalance, event))
-  }
 
-  private def updateBalanceAndAtm(newBalance: Int, event: Event) = event match {
-    case w: Withdrawal => w.copy(balance = newBalance, atm = name)
-    case o: OutOfMoney => o.copy(balance = 0, atm = name)
-    case n: NotEnoughMoney => n.copy(atm = name)
-    case e: Event => e
+    def sendToOutputAndAck(e: Event): Unit = {
+      implicit val timeout: Timeout = Timeout(30 seconds)
+      Await.result(output ? e, 30 seconds)
+      sender() ! Done
+    }
 
+    def updateBalanceAndAtm(newBalance: Int, event: Event) = event match {
+      case w: Withdrawal => w.copy(balance = newBalance, atm = name)
+      case o: OutOfMoney => o.copy(atm = name)
+      case n: NotEnoughMoney => n.copy(atm = name)
+      case _: Event => throw new IllegalStateException("Event is not a correct event")
+    }
   }
 
   private def name = self.path.name
 }
 
-sealed class Event(val time: Instant, val eventType: String, val atm: String, val balance: Int)
+sealed class Event(val time: Instant, val eventType: String, val atm: String)
 
 case class Withdrawal(override val time: Instant,
                       amount: Int,
                       override val atm: String = "unknown",
-                      override val balance: Int = 0,
-                      override val eventType: String = "withdrawal") extends Event(time, eventType, atm, balance)
+                      override val eventType: String = "withdrawal",
+                      balance: Int = 0) extends Event(time, eventType, atm)
 
 case class OutOfMoney(override val time: Instant,
                       override val atm: String = "unknown",
-                      override val balance: Int = 0,
-                      override val eventType: String = "out-of-money") extends Event(time, eventType, atm, balance)
+                      override val eventType: String = "out-of-money") extends Event(time, eventType, atm)
 
 case class NotEnoughMoney(override val time: Instant,
                           override val atm: String = "unknown",
-                          override val balance: Int = 0,
-                          override val eventType: String = "not-enough-money") extends Event(time, eventType, atm, balance)
+                          override val eventType: String = "not-enough-money") extends Event(time, eventType, atm)
 
